004c94b337c83fbbc6504e8e897141e76b149404
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-10-28 22:09:55 +0200

    input invalidation added

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,11 +1,30 @@
 import classes from "./MainPage.module.css";
+import useInput from "../hooks/use-input";
 
 const Main = () => {
+  const {
+    inputValue: nickName,
+    inputIsValid: nickNameIsValid,
+    inputHasError: nickNameHasError,
+    inputChangeHandler: nickNameChangeHandler,
+    inputBlurHandler: nickNameBlurHandler,
+    reset: nickNameReset,
+  } = useInput((nickName: string) => nickName !== "");
+
+  const nickNameInputClasses = nickNameHasError ? classes["invalid"] : "";
+
   return (
     <main className={classes["main-page"]}>
       <form className={classes["main-page--form"]}>
         <label htmlFor="nickname">Nickname</label>
-        <input id="nickname" type="text"></input>
+        <input
+          id="nickname"
+          type="text"
+          className={nickNameInputClasses}
+          onChange={nickNameChangeHandler}
+          onBlur={nickNameBlurHandler}
+          value={nickName}
+        ></input>
         <label htmlFor="game-type-select">Game Type</label>
         <select name="game-type" id="game-type-select">
           <option value="quiz">Quiz</option>
