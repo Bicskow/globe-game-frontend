2365c0ff2190533d66247ac62113acdd74ca766f
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-11-10 22:00:52 +0100

    timer hook use effect fix

--- src/hooks/use-timer.ts
+++ src/hooks/use-timer.ts
@@ -4,17 +4,24 @@ const useTimer = () => {
   const pad = (n: number) => {
     return ("0" + n).slice(-2);
   };
-  const timerRunning = true;
-  const startTime = new Date();
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [startTime, setStartTime] = useState(new Date().getTime());
   const [timer, setTimer] = useState("00:00:00");
 
-  const startTimer = () => {};
+  const startTimer = () => {
+    setTimerRunning(true);
+    setStartTime(new Date().getTime());
+  };
+
+  const stopTimer = () => {
+    setTimerRunning(false);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (timerRunning) {
-        const currentTime = new Date();
-        const timeDiff = new Date(currentTime.getTime() - startTime.getTime());
+        const currentTime = new Date().getTime();
+        const timeDiff = new Date(currentTime - startTime);
         setTimer(
           `${pad(timeDiff.getMinutes())}:${pad(timeDiff.getUTCSeconds())}:${pad(
             Math.floor(timeDiff.getMilliseconds() / 16.66667)
@@ -24,11 +31,12 @@ const useTimer = () => {
     }, 1);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [timerRunning, startTime]);
 
   return {
     timer,
     startTimer,
+    stopTimer,
   };
 };
 
