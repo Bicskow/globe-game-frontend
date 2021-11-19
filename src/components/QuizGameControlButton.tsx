d175896cae9aefd3e75bc4bd38a216f1170e1bf5
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-11-19 23:04:12 +0100

    button highlighting added

--- src/components/QuizGameControlButton.tsx
+++ src/components/QuizGameControlButton.tsx
@@ -1,7 +1,36 @@
 import classes from "./QuizGameControlButton.module.css";
+import { useState } from "react";
+import { useAppSelector } from "../hooks/redux-hooks";
+import { QuizQuestion } from "../models/Question";
 
-const QuizGameControlButton: React.FC<{ name: string }> = (props) => {
-  return <button className={classes.quizButton}>{props.name}</button>;
+const QuizGameControlButton: React.FC<{
+  name: string;
+}> = (props) => {
+  const { questions, currentQuestion, gameStarted } = useAppSelector(
+    (state) => state.game
+  );
+  const [highlightClass, setHighlightClass] = useState("");
+
+  const handleButtonClicked = () => {
+    if (gameStarted) {
+      if (
+        props.name ===
+        (questions[currentQuestion] as QuizQuestion).correctAnswer
+      ) {
+        setHighlightClass(classes.correct);
+      } else {
+        setHighlightClass(classes.wrong);
+      }
+    }
+  };
+  return (
+    <button
+      className={`${classes.quizButton} ${highlightClass}`}
+      onClick={handleButtonClicked}
+    >
+      {props.name}
+    </button>
+  );
 };
 
 export default QuizGameControlButton;
