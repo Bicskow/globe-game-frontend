2f5fb3fa99f69c6505ff542d6077719e8555aae1
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-11-21 16:09:56 +0100

    game result added

--- src/components/GameResults.tsx
+++ src/components/GameResults.tsx
@@ -1,10 +1,27 @@
 import Card from "./Card";
 import classes from "./GameResults.module.css";
+import { useAppSelector } from "../hooks/redux-hooks";
+import { Fragment } from "react";
 
 const GameResults = () => {
+  const { questions } = useAppSelector((state) => state.game);
   return (
     <Card className={classes.results}>
-      <div>Results</div>
+      <Fragment>
+        <div className={classes.headerRow}>Country Name</div>
+        <div className={classes.headerRow}>Time</div>
+        <div className={classes.headerRow}>Answer</div>
+        <div className={classes.headerDivider}></div>
+        {questions.map((question) => {
+          return (
+            <Fragment>
+              <div>{question.correctAnswer}</div>
+              <div>00:00:00</div>
+              <div>{question.answerIsCorrect ? "OK" : "NOK"}</div>
+            </Fragment>
+          );
+        })}
+      </Fragment>
     </Card>
   );
 };
