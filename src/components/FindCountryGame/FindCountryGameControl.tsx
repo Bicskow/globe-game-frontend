8d931e9447840cda674c9e0a78688a857ec797d8
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-11-28 07:00:26 +0100

    find country game control added

--- src/components/FindCountryGame/FindCountryGameControl.tsx
+++ src/components/FindCountryGame/FindCountryGameControl.tsx
@@ -3,19 +3,37 @@ import classes from "./FindCountryGameControl.module.css";
 import { Fragment } from "react";
 import { useAppSelector } from "../../hooks/redux-hooks";
 import { GameStep } from "../../store";
+import FindCountryGameControlButton from "./FindCountryGameControlButton";
 
 const FindCountryGameControl = () => {
   const { questions, currentQuestion, gameStep } = useAppSelector(
     (state) => state.game
   );
+
+  const getQuestionList = () => {
+    let toReturn = [];
+    for (let i = currentQuestion; i >= 0; i--) {
+      console.log(i);
+      toReturn.push(questions[i]);
+    }
+    return toReturn;
+  };
+
   return (
     <Card className={classes.findCountryGameControl}>
       <Fragment>
-        <p>Select the country:</p>
-        <div>
-          {gameStep === GameStep.InGame &&
-            questions[currentQuestion].correctAnswer}
-        </div>
+        {gameStep === GameStep.InGame && (
+          <div className={classes.findCountryList}>
+            {getQuestionList().map((question, index) => (
+              <FindCountryGameControlButton
+                answerOk={question.answerIsCorrect}
+                key={index}
+                name={question.correctAnswer}
+              ></FindCountryGameControlButton>
+            ))}
+            <div className={classes.listCoverBottom}></div>
+          </div>
+        )}
       </Fragment>
     </Card>
   );
