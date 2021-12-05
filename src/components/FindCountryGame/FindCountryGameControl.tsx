a61d25e29ecce4a73fac6f783a52073d669df2a6
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2021-12-05 23:08:52 +0100

    sliding control added

--- src/components/FindCountryGame/FindCountryGameControl.tsx
+++ src/components/FindCountryGame/FindCountryGameControl.tsx
@@ -1,6 +1,6 @@
 import Card from "../Card";
 import classes from "./FindCountryGameControl.module.css";
-import { Fragment, useEffect, useRef } from "react";
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 import { useAppSelector } from "../../hooks/redux-hooks";
 import { GameStep } from "../../store";
 import FindCountryGameControlButton from "./FindCountryGameControlButton";
@@ -9,29 +9,48 @@ const FindCountryGameControl = () => {
   const { questions, currentQuestion, gameStep } = useAppSelector(
     (state) => state.game
   );
+  const [shiftList, setShiftList] = useState(0);
+
+  const getShift = useCallback(() => {
+    let shiftCurrent = questions.length - currentQuestion - 1;
+    let shift = 0;
+    for (let i = 0; i < itemsRef.current.length && i < shiftCurrent; i++) {
+      shift += itemsRef.current[i] ?? 0;
+      shift += 16;
+    }
+    return shift + 6;
+  }, [currentQuestion, questions.length]);
 
   const itemsRef = useRef<Array<number | undefined>>([]);
 
   useEffect(() => {
-    console.log(itemsRef);
-  }, [currentQuestion]);
+    setShiftList(getShift());
+  }, [currentQuestion, getShift]);
 
   return (
     <Card className={classes.findCountryGameControl}>
       <Fragment>
         {gameStep === GameStep.InGame && (
           <div className={classes.findCountryList}>
-            <ul>
-              {questions.map((question, index) => (
-                <FindCountryGameControlButton
-                  ref={(el: number | undefined) =>
-                    (itemsRef.current[index] = el)
-                  }
-                  answerOk={question.answerIsCorrect}
-                  key={index}
-                  name={question.correctAnswer}
-                ></FindCountryGameControlButton>
-              ))}
+            <ul
+              style={{
+                transform: `translateY(-${shiftList}px)`,
+                transition: "transform ease-out 1s",
+              }}
+            >
+              {questions
+                .slice()
+                .reverse()
+                .map((question, index) => (
+                  <FindCountryGameControlButton
+                    ref={(el: number | undefined) =>
+                      (itemsRef.current[index] = el)
+                    }
+                    answerOk={question.answerIsCorrect}
+                    key={index}
+                    name={question.correctAnswer}
+                  ></FindCountryGameControlButton>
+                ))}
             </ul>
             <div className={classes.listCoverBottom}></div>
           </div>
